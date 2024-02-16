174db895bb1643445f488fd27b9be8d53c1cf477
AdrianP <adrianpalaciosaguilar@outlook.es>
2024-02-16 18:22:15 -0500

    Vista de formularios, alumno, profesor, admin

---
+++ src/js/form-alumno.js
@@ -0,0 +1,26 @@
+import './../css/style.css';
+
+document.getElementById("register").addEventListener("click", function() {
+        console.log("click");
+        document.getElementById("registerForm").classList.remove("hidden");
+        document.getElementById("loginForm").classList.add("hidden");
+
+        document.querySelector("h1").innerHTML = "Registrar";
+        
+        document.getElementById("register").classList.add("bg-[#3B3355]");
+        document.getElementById("register").classList.add("text-white");
+        document.getElementById("login").classList.remove("text-white");
+        document.getElementById("login").classList.remove("bg-[#3B3355]");
+});
+
+document.getElementById("login").addEventListener("click", function() {
+    document.getElementById("registerForm").classList.add("hidden");
+    document.getElementById("loginForm").classList.remove("hidden");
+    
+    document.querySelector("h1").innerHTML = "Iniciar sesión";
+
+    document.getElementById("register").classList.remove("bg-[#3B3355]");
+    document.getElementById("register").classList.remove("text-white");
+    document.getElementById("login").classList.add("text-white");
+    document.getElementById("login").classList.add("bg-[#3B3355]");
+});
--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,13 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'src/index.html'),
         admin: resolve(__dirname, 'src/pages/admin-prueba.html'),
-        prueba: resolve(__dirname, 'src/pages/prueba.html')
+        prueba: resolve(__dirname, 'src/pages/prueba.html'),
+        form_alumno: resolve(__dirname, 'src/pages/form-alumno.html'),
+        form_profesor: resolve(__dirname, 'src/pages/form-profesor.html'),
+        form_admin: resolve(__dirname, 'src/pages/form-admin.html'),
+        vista_alumno: resolve(__dirname, 'src/pages/vista-alumno.html'),
+        vista_profesor: resolve(__dirname, 'src/pages/vista-profesor.html'),
+        vista_admin: resolve(__dirname, 'src/pages/vista-admin.html')
       }
     }
   }
